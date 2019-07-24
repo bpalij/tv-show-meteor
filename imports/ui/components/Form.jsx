5eec98a3b1ee2bf6420a20aa379559d6865e7260
bpalij <bpalij@gmail.com>
2019-07-24 14:29:54 +0300

    Removing Meteor imports
    Not required with configured eslint env

--- imports/ui/components/Form.jsx
+++ imports/ui/components/Form.jsx
@@ -1,4 +1,4 @@
-import { Meteor } from 'meteor/meteor';
+// import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import headersToObj from '../../misc/headersToObj.js';
--- imports/ui/components/ReduxApp.jsx
+++ imports/ui/components/ReduxApp.jsx
@@ -1,4 +1,4 @@
-import { Meteor } from 'meteor/meteor';
+// import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';

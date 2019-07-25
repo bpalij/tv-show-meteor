d56b528cad5bfbcf4f2f47a795d51e411b96fc86
bpalij <bpalij@gmail.com>
2019-07-25 18:51:35 +0300

    Still not working

--- imports/ui/components/Paginator.jsx
+++ imports/ui/components/Paginator.jsx
@@ -1,9 +1,9 @@
-// import { Meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // es-lint-disable-next-line no-unused-vars
-import Links from '../../api/links.js';
-// import '../../methods.js';
+// import Links from '../../api/links.js';
+import '../../methods.js';
 // import headersToObj from '../../misc/headersToObj.js';
 // import getImageLinks from '../../misc/getImageLinks.js';
 // import paramsQuery from '../../misc/paramsQuery.js';

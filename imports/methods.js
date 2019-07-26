3a600610a622a00899eefdc0f607703793521160
bpalij <bpalij@gmail.com>
2019-07-26 14:34:26 +0300

    Trying minimum method
    Does not work

--- imports/methods.js
+++ imports/methods.js
@@ -1,3 +1,36 @@
+// import { Meteor } from 'meteor/meteor';
+// import headersToObj from './misc/headersToObj.js';
+// import getImageLinks from './misc/getImageLinks.js';
+// import paramsQuery from './misc/paramsQuery.js';
+
+// Meteor.methods({
+//   getInfo (filters, page) {
+//     let headers;
+//     let dataTemp;
+//     return fetch(`https://api.trakt.tv/shows/${paramsQuery(filters, page)}`, {
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'trakt-api-version': '2',
+//         'trakt-api-key': `${Meteor.settings.traktClientId}`,
+//       },
+//     })
+//       .then((res) => {
+//         if (res.ok) {
+//           headers = headersToObj(res.headers);
+//           return res.json();
+//         }
+//         throw new Error(`Error ${res.status}: ${res.statusText}`);
+//       })
+//       .then((data) => {
+//         dataTemp = data;
+//         return getImageLinks(data);
+//       })
+//       .then(images => ({ data: dataTemp, headers, images }))
+//       .catch((e) => { throw new Meteor.Error(e); });
+//   },
+// });
+
+
 import { Meteor } from 'meteor/meteor';
 import headersToObj from './misc/headersToObj.js';
 import getImageLinks from './misc/getImageLinks.js';
@@ -5,27 +38,6 @@ import paramsQuery from './misc/paramsQuery.js';
 
 Meteor.methods({
   getInfo (filters, page) {
-    let headers;
-    let dataTemp;
-    return fetch(`https://api.trakt.tv/shows/${paramsQuery(filters, page)}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'trakt-api-version': '2',
-        'trakt-api-key': `${Meteor.settings.traktClientId}`,
-      },
-    })
-      .then((res) => {
-        if (res.ok) {
-          headers = headersToObj(res.headers);
-          return res.json();
-        }
-        throw new Error(`Error ${res.status}: ${res.statusText}`);
-      })
-      .then((data) => {
-        dataTemp = data;
-        return getImageLinks(data);
-      })
-      .then(images => ({ data: dataTemp, headers, images }))
-      .catch((e) => { throw new Meteor.Error(e); });
+    console.log('work');
   },
 });

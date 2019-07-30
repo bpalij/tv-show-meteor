8cf6b35d0a272b84d18c695771dc376dc1d50a01
bpalij <bpalij@gmail.com>
2019-07-30 11:11:18 +0300

    Remaking methods from promises to async

--- imports/methods.js
+++ imports/methods.js
@@ -5,7 +5,7 @@ import getImageLinks from './misc/getImageLinks.js';
 import paramsQuery from './misc/paramsQuery.js';
 
 Meteor.methods({
-  getInfo (filters, page) {
+  async getInfo (filters, page) {
     check(page, Match.Maybe(Match.OneOf(Match.Integer, String)));
     check(filters, Match.ObjectIncluding(
       {
@@ -14,27 +14,25 @@ Meteor.methods({
         year: Match.Maybe(Match.OneOf(Match.Integer, String)),
       },
     ));
-    let headers;
-    let dataTemp;
-    return fetch(`https://api.trakt.tv/shows/${paramsQuery(filters, page)}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'trakt-api-version': '2',
-        'trakt-api-key': `${Meteor.settings.traktClientId}`,
-      },
-    })
-      .then((res) => {
-        if (res.ok) {
-          headers = headersToObj(res.headers);
-          return res.json();
-        }
-        throw new Error(`Error ${res.status}: ${res.statusText}`);
-      })
-      .then((data) => {
-        dataTemp = data;
-        return getImageLinks(data);
-      })
-      .then(images => ({ data: dataTemp, headers, images }))
-      .catch((e) => { throw new Meteor.Error(e); });
+    try {
+      let headers;
+      let data;
+      const res = await fetch(`https://api.trakt.tv/shows/${paramsQuery(filters, page)}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'trakt-api-version': '2',
+          'trakt-api-key': `${Meteor.settings.traktClientId}`,
+        },
+      });
+      if (res.ok) {
+        headers = headersToObj(res.headers);
+        data = await res.json();
+        const images = await getImageLinks(data);
+        return { data, headers, images };
+      }
+      throw new Error(`Error ${res.status}: ${res.statusText}`);
+    } catch (e) {
+      throw new Meteor.Error(e);
+    }
   },
 });

18162a5ce5bab7ca69bcf3647b767cd4525d947f
bpalij <bpalij@gmail.com>
2019-07-30 13:54:03 +0300

    Moving function from methods

--- imports/methods.js
+++ imports/methods.js
@@ -4,35 +4,37 @@ import headersToObj from './misc/headersToObj.js';
 import getImageLinks from './misc/getImageLinks.js';
 import paramsQuery from './misc/paramsQuery.js';
 
-Meteor.methods({
-  async getInfo (filters, page) {
-    check(page, Match.Maybe(Match.OneOf(Match.Integer, String)));
-    check(filters, Match.ObjectIncluding(
-      {
-        sort: String,
-        query: Match.Maybe(String),
-        year: Match.Maybe(Match.OneOf(Match.Integer, String)),
+async function getInfo (filters, page) {
+  check(page, Match.Maybe(Match.OneOf(Match.Integer, String)));
+  check(filters, Match.ObjectIncluding(
+    {
+      sort: String,
+      query: Match.Maybe(String),
+      year: Match.Maybe(Match.OneOf(Match.Integer, String)),
+    },
+  ));
+  try {
+    let headers;
+    let data;
+    const res = await fetch(`https://api.trakt.tv/shows/${paramsQuery(filters, page)}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': `${Meteor.settings.traktClientId}`,
       },
-    ));
-    try {
-      let headers;
-      let data;
-      const res = await fetch(`https://api.trakt.tv/shows/${paramsQuery(filters, page)}`, {
-        headers: {
-          'Content-Type': 'application/json',
-          'trakt-api-version': '2',
-          'trakt-api-key': `${Meteor.settings.traktClientId}`,
-        },
-      });
-      if (res.ok) {
-        headers = headersToObj(res.headers);
-        data = await res.json();
-        const images = await getImageLinks(data);
-        return { data, headers, images };
-      }
-      throw new Error(`Error ${res.status}: ${res.statusText}`);
-    } catch (e) {
-      throw new Meteor.Error(e);
+    });
+    if (res.ok) {
+      headers = headersToObj(res.headers);
+      data = await res.json();
+      const images = await getImageLinks(data);
+      return { data, headers, images };
     }
-  },
+    throw new Error(`Error ${res.status}: ${res.statusText}`);
+  } catch (e) {
+    throw new Meteor.Error(e);
+  }
+}
+
+Meteor.methods({
+  getInfo,
 });
